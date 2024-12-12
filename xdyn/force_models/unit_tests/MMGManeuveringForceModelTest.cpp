63f61130a467331c5fdd9b30193f278a8102de14
Florent Bickert <florent.bickert@ec-nantes.fr>
2024-12-12 15:56:24 +0100

    ✅ Updated the tests for 4 DOF MMG maneuvering model
    (cherry picked from commit aac877ae95f8fba29777fe0e8e17f1f5b975c38e)

--- xdyn/force_models/unit_tests/MMGManeuveringForceModelTest.cpp
+++ xdyn/force_models/unit_tests/MMGManeuveringForceModelTest.cpp
@@ -1,6 +1,8 @@
 #include "MMGManeuveringForceModelTest.hpp"
 #include "MMGManeuveringForceModel.hpp"
 
+#define PI M_PI
+
 MMGManeuveringForceModelTest::MMGManeuveringForceModelTest()
 {
 }
@@ -9,8 +11,8 @@ MMGManeuveringForceModelTest::~MMGManeuveringForceModelTest()
 {
 }
 
-std::string get_input();
-std::string get_input()
+std::string get_input_3dof();
+std::string get_input_3dof()
 {
     std::stringstream ss; // Data from H. Yasukawa and Y. Yoshimura, “Introduction of MMG standard method for ship maneuvering predictions,” Journal of Marine Science and Technology, vol. 20, no. 1, pp. 37–52, Nov. 2014, doi: 10.1007/s00773-014-0293-y
     ss << "calculation point in body frame:\n"
@@ -37,37 +39,119 @@ std::string get_input()
         <<"Nvrr: 0.055\n"
         <<"Nrrr: -0.013\n"
         <<"mx: 0.022\n"
-        <<"my: 0.223";
+        <<"my: 0.223\n"
+        <<"GM: 0\n"
+        <<"Xvphi: 0\n"
+        <<"Xrphi: 0\n"
+        <<"Xphiphi: 0\n"
+        <<"Yphi: 0\n"
+        <<"Yphivv: 0\n"
+        <<"Yvphiphi: 0\n"
+        <<"Yphirr: 0\n"
+        <<"Yrphiphi: 0\n"
+        <<"Nphi: 0\n"
+        <<"Nphivv: 0\n"
+        <<"Nvphiphi: 0\n"
+        <<"Nphirr: 0\n"
+        <<"Nrphiphi: 0\n"
+        <<"Kphi: 0\n"
+        <<"Kphiphi: 0\n";
+
+    return ss.str();
+}
+
+std::string get_input_4dof();
+std::string get_input_4dof()
+{
+    std::stringstream ss; // Data from R. Okuda, H. Yasukawa, and A. Matsuda, “Validation of maneuvering simulations for a KCS at different forward speeds using the 4-DOF MMG method,” Ocean Engineering, vol. 284, p. 115174, Sep. 2023, doi: 10.1016/j.oceaneng.2023.115174.
+    ss << "calculation point in body frame:\n"
+        <<"    x: {value: -3.39, unit: m}\n"
+        <<"    y: {value: 0, unit: m}\n"
+        <<"    z: {value: -2.7, unit: m}\n"
+        <<"Lpp: {value: 230, unit: m}\n"
+        <<"T: {value: 10.8, unit: m}\n"
+        <<"R0: 0.02\n"
+        <<"Xvv: -0.087\n"
+        <<"Xvr: -0.093\n"
+        <<"Xrr: -0.023\n"
+        <<"Xvvvv: 0.809\n"
+        <<"Yv: -0.243\n"
+        <<"Yr: 0.055\n"
+        <<"Yvvv: -1.675\n"
+        <<"Yrvv: -0.083\n"
+        <<"Yvrr: -0.447\n"
+        <<"Yrrr: 0.02\n"
+        <<"Nv: -0.105\n"
+        <<"Nr: -0.036\n"
+        <<"Nvvv: -0.288\n"
+        <<"Nrvv: -0.579\n"
+        <<"Nvrr: -0.003\n"
+        <<"Nrrr: -0.042\n"
+        <<"mx: 0.006\n"
+        <<"my: 0.152\n"
+        <<"GM: 0.6\n"
+        <<"Xvphi: 0.01\n"
+        <<"Xrphi: 0.009\n"
+        <<"Xphiphi: -0.002\n"
+        <<"Yphi: 0.001\n"
+        <<"Yphivv: 0.161\n"
+        <<"Yvphiphi: -0.243\n"
+        <<"Yphirr: 0\n"// unused in the article
+        <<"Yrphiphi: -0.13\n"
+        <<"Nphi: -0.009\n"
+        <<"Nphivv: -0.327\n"
+        <<"Nvphiphi: -0.232\n"
+        <<"Nphirr: 0\n"// unused in the article
+        <<"Nrphiphi: 0.041\n"
+        <<"Kphi: -35295092\n"
+        <<"Kphiphi: -897604071\n";
+
     return ss.str();
 }
 
 TEST_F(MMGManeuveringForceModelTest, can_parse)
 {
-    const auto data = MMGManeuveringForceModel::parse(get_input());
-    ASSERT_DOUBLE_EQ(data.application_point.x, -11.1);
+    const auto data = MMGManeuveringForceModel::parse(get_input_4dof());
+    ASSERT_DOUBLE_EQ(data.application_point.x, -3.39);
     ASSERT_DOUBLE_EQ(data.application_point.y, 0);
-    ASSERT_DOUBLE_EQ(data.application_point.z, 0);
-    ASSERT_DOUBLE_EQ(data.Lpp, 320);
-    ASSERT_DOUBLE_EQ(data.T, 20.8);
-    ASSERT_DOUBLE_EQ(data.R0, 0.022);
-    ASSERT_DOUBLE_EQ(data.Xvv, -0.04);
-    ASSERT_DOUBLE_EQ(data.Xrr, 0.011);
-    ASSERT_DOUBLE_EQ(data.Xvr, 0.002);
-    ASSERT_DOUBLE_EQ(data.Xvvvv, 0.771);
-    ASSERT_DOUBLE_EQ(data.Yv, -0.315);
-    ASSERT_DOUBLE_EQ(data.Yr, 0.083);
-    ASSERT_DOUBLE_EQ(data.Yvvv, -1.607);
-    ASSERT_DOUBLE_EQ(data.Yrvv, 0.379);
-    ASSERT_DOUBLE_EQ(data.Yvrr, -0.391);
-    ASSERT_DOUBLE_EQ(data.Yrrr, 0.008);
-    ASSERT_DOUBLE_EQ(data.Nv, -0.137);
-    ASSERT_DOUBLE_EQ(data.Nr, -0.049);
-    ASSERT_DOUBLE_EQ(data.Nvvv, -0.03);
-    ASSERT_DOUBLE_EQ(data.Nrvv, -0.294);
-    ASSERT_DOUBLE_EQ(data.Nvrr, 0.055);
-    ASSERT_DOUBLE_EQ(data.Nrrr, -0.013);
-    ASSERT_DOUBLE_EQ(data.mx, 0.022);
-    ASSERT_DOUBLE_EQ(data.my, 0.223);
+    ASSERT_DOUBLE_EQ(data.application_point.z, -2.7);
+    ASSERT_DOUBLE_EQ(data.Lpp, 230);
+    ASSERT_DOUBLE_EQ(data.T, 10.8);
+    ASSERT_DOUBLE_EQ(data.R0, 0.02);
+    ASSERT_DOUBLE_EQ(data.Xvv, -0.087);
+    ASSERT_DOUBLE_EQ(data.Xrr, -0.023);
+    ASSERT_DOUBLE_EQ(data.Xvr, -0.093);
+    ASSERT_DOUBLE_EQ(data.Xvvvv, 0.809);
+    ASSERT_DOUBLE_EQ(data.Yv, -0.243);
+    ASSERT_DOUBLE_EQ(data.Yr, 0.055);
+    ASSERT_DOUBLE_EQ(data.Yvvv,  -1.675);
+    ASSERT_DOUBLE_EQ(data.Yrvv, -0.083);
+    ASSERT_DOUBLE_EQ(data.Yvrr, -0.447);
+    ASSERT_DOUBLE_EQ(data.Yrrr, 0.02);
+    ASSERT_DOUBLE_EQ(data.Nv, -0.105);
+    ASSERT_DOUBLE_EQ(data.Nr, -0.036);
+    ASSERT_DOUBLE_EQ(data.Nvvv, -0.288);
+    ASSERT_DOUBLE_EQ(data.Nrvv, -0.579);
+    ASSERT_DOUBLE_EQ(data.Nvrr, -0.003);
+    ASSERT_DOUBLE_EQ(data.Nrrr, -0.042);
+    ASSERT_DOUBLE_EQ(data.mx, 0.006);
+    ASSERT_DOUBLE_EQ(data.my, 0.152);
+    ASSERT_DOUBLE_EQ(data.GM, 0.6);
+    ASSERT_DOUBLE_EQ(data.Xvphi, 0.01);
+    ASSERT_DOUBLE_EQ(data.Xrphi, 0.009);
+    ASSERT_DOUBLE_EQ(data.Xphiphi, -0.002);
+    ASSERT_DOUBLE_EQ(data.Yphi, 0.001);
+    ASSERT_DOUBLE_EQ(data.Yphivv, 0.161);
+    ASSERT_DOUBLE_EQ(data.Yvphiphi, -0.243);
+    ASSERT_DOUBLE_EQ(data.Yphirr, 0.);
+    ASSERT_DOUBLE_EQ(data.Yrphiphi, -0.13);
+    ASSERT_DOUBLE_EQ(data.Nphi, -0.009);
+    ASSERT_DOUBLE_EQ(data.Nphivv, -0.327);
+    ASSERT_DOUBLE_EQ(data.Nvphiphi, -0.232);
+    ASSERT_DOUBLE_EQ(data.Nphirr, 0.);
+    ASSERT_DOUBLE_EQ(data.Nrphiphi, 0.041);
+    ASSERT_DOUBLE_EQ(data.Kphi, -35295092);
+    ASSERT_DOUBLE_EQ(data.Kphiphi, -897604071);
 }
 
 BodyStates get_states();
@@ -80,23 +164,26 @@ BodyStates get_states()
     states.z.record(0, 0);
     states.u.record(0, 1);
     states.v.record(0, 2);
-    states.w.record(0, 0);
-    states.p.record(0, 0);
-    states.q.record(0, 0);
+    states.w.record(0, -.5);
+    states.p.record(0, 1);
+    states.q.record(0, -2);
     states.r.record(0, 3);
     states.qr.record(0, 1);
     states.qi.record(0, 0);
     states.qj.record(0, 0);
-    states.qr.record(0, 0);
+    states.qk.record(0, 0);
     return states;
 }
 
-TEST_F(MMGManeuveringForceModelTest, example)
+TEST_F(MMGManeuveringForceModelTest, example_3dof)
+/*
+    Verification of the 3 DOF case with KVLCC input data
+*/
 {
     EnvironmentAndFrames env;
     env.rho = 1000;
     env.rot = YamlRotation("angle", {"z","y'","x''"});
-    auto force_model = MMGManeuveringForceModel(MMGManeuveringForceModel::parse(get_input()), "body", env);
+    auto force_model = MMGManeuveringForceModel(MMGManeuveringForceModel::parse(get_input_3dof()), "body", env);
     auto states = get_states();
     auto F = force_model.get_force(states, 0, env, {});
     ASSERT_DOUBLE_EQ(F.X(), 35918728522.330902-500*320*20.8*0.022*(1+pow((2-11.1*3),2))+0.223*500*320*320*20.8*(2-11.1*3)*3);
@@ -105,4 +192,93 @@ TEST_F(MMGManeuveringForceModelTest, example)
     ASSERT_DOUBLE_EQ(F.K(), 0);
     ASSERT_DOUBLE_EQ(F.M(), 0);
     ASSERT_DOUBLE_EQ(F.N(), -455883858001617.88);
+}
+
+TEST_F(MMGManeuveringForceModelTest, example_4dof)
+/*
+    Verification of the 4 DOF case with KCS input data
+*/
+{
+    MMGManeuveringForceModel::Input input = MMGManeuveringForceModel::parse(get_input_4dof());
+    EnvironmentAndFrames env;
+    env.rho = 1000;
+    env.g=9.81;
+    env.rot = YamlRotation("angle", {"z","y'","x''"});
+    // Define states
+    BodyStates states;
+    states.convention = YamlRotation("angle", {"z","y'","x''"});
+    states.u.record(0, 10);
+    states.v.record(0, -2);
+    states.r.record(0, 1);
+
+    // Define a body mass
+    states.solid_body_inertia(2,2)=52043000;
+
+    ////////////////////////////////
+    // TEST1 : no phi, no dphi_dt //
+    ////////////////////////////////
+    
+    auto force_model = MMGManeuveringForceModel(input, "body", env);
+    auto F1 = force_model.get_force(states, 0, env, {});
+    ASSERT_DOUBLE_EQ(F1.X(), -1601757553.1071172);
+    ASSERT_DOUBLE_EQ(F1.Y(), 40686696096.544792);
+    ASSERT_DOUBLE_EQ(F1.Z(), 0);
+    ASSERT_DOUBLE_EQ(F1.K(), 0);
+    ASSERT_DOUBLE_EQ(F1.M(), 0);
+    ASSERT_DOUBLE_EQ(F1.N(), -12949573903321.889);
+
+    ///////////////////////////////////
+    // TEST 2 : phi=-30°, no dphi_dt //
+    ///////////////////////////////////
+
+    const double phi=-PI/6;
+    // Get quaternions corresponding corresponding to a 30° heel angle
+    auto quaternions=states.convert(ssc::kinematics::EulerAngles(phi,0,0),env.rot);
+    states.qr.record(0,std::get<0>(quaternions));
+    states.qi.record(0,std::get<1>(quaternions));
+    states.qj.record(0,std::get<2>(quaternions));
+    states.qk.record(0,std::get<3>(quaternions));
+
+    // Compute some quantities for the comparison
+    double U_target=sqrt(100+pow(5.39,2));//vm=-2-3.39*1=-5.39
+    double dimension_const=500*pow(U_target,2)*230*10.8;//0.5*rho*U*U*Lpp*d
+
+    auto F2 = force_model.get_force(states, 0, env, {});
+    ASSERT_DOUBLE_EQ(F2.X(), -1601757553.1071172+(0.01*-5.39/U_target*phi+0.009*230/U_target*phi-0.002*phi*phi)*dimension_const);
+    ASSERT_DOUBLE_EQ(F2.Y(), 40686696096.544792+(0.001*phi+0.161*phi*pow(5.39/U_target,2)-0.243*phi*phi*(-5.39)/U_target-0.13*230/U_target*phi*phi)*dimension_const);
+    ASSERT_DOUBLE_EQ(F2.Z(), 0);
+    ASSERT_DOUBLE_EQ(F2.K(), 52043000*9.81*0.6*PI/6);
+    ASSERT_DOUBLE_EQ(F2.M(), 0);
+    ASSERT_DOUBLE_EQ(F2.N(), -12949573903321.889+(-0.009*phi-0.327*phi*pow(5.39/U_target,2)-0.232*phi*phi*(-5.39)/U_target+0.041*230/U_target*phi*phi)*dimension_const*230);
+    
+    /////////////////////////////////////////////
+    // TEST 3 : phi=-30°,  dphi_dt=-3 and zG=0 //
+    /////////////////////////////////////////////
+
+    states.p.record(0, -.5);// define dphi_dt=-0.5
+    input.application_point.z=0; // to have zG=0 and thus the same vm than TEST 2
+    force_model = MMGManeuveringForceModel(input, "body", env);
+    auto F3 = force_model.get_force(states, 0, env, {});
+    ASSERT_DOUBLE_EQ(F3.X(), F2.X());
+    ASSERT_DOUBLE_EQ(F3.Y(), F2.Y());
+    ASSERT_DOUBLE_EQ(F3.Z(), 0);
+    ASSERT_DOUBLE_EQ(F3.K(), 52043000*9.81*0.6*PI/6+35295092*.5+897604071*.25);
+    ASSERT_DOUBLE_EQ(F3.M(), 0);
+    ASSERT_DOUBLE_EQ(F3.N(), F2.N());
+
+    ///////////////////////////////////////////
+    // TEST 4 : phi=-30°,  dphi_dt<>0, zG<>0 //
+    ///////////////////////////////////////////
+
+    // xG and zG are modified to have the same vm than TEST 2
+    input.application_point.x-=1; 
+    input.application_point.z=2;
+    force_model = MMGManeuveringForceModel(input, "body", env);
+    auto F4 = force_model.get_force(states, 0, env, {});
+    ASSERT_DOUBLE_EQ(F4.X(), F2.X());
+    ASSERT_DOUBLE_EQ(F4.Y(), F2.Y());
+    ASSERT_DOUBLE_EQ(F4.Z(), 0);
+    ASSERT_DOUBLE_EQ(F4.K(), F3.K());
+    ASSERT_DOUBLE_EQ(F4.M(), 0);
+    ASSERT_DOUBLE_EQ(F4.N(), F2.N());
 }
