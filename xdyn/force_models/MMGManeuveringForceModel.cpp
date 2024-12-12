770baf0f7ec43740662f87e67097485c9eddb7b0
Florent Bickert <florent.bickert@ec-nantes.fr>
2024-12-12 15:54:29 +0100

    ✨ Added the roll dependency to the MMG maneuvering model
    (cherry picked from commit 04f14c3cc52ae6498584ab5d2529c838c9c4d8e4)

--- xdyn/force_models/MMGManeuveringForceModel.cpp
+++ xdyn/force_models/MMGManeuveringForceModel.cpp
@@ -18,20 +18,36 @@ MMGManeuveringForceModel::Input::Input():
         Xrr(0.0),
         Xvr(0.0),
         Xvvvv(0.0),
+        Xvphi(0.0),
+        Xrphi(0.0),
+        Xphiphi(0.0),
         Yv(0.0),
         Yr(0.0),
         Yvvv(0.0),
         Yrvv(0.0),
         Yvrr(0.0),
         Yrrr(0.0),
+        Yphi(0.0),
+        Yphivv(0.0),
+        Yvphiphi(0.0),
+        Yphirr(0.0),
+        Yrphiphi(0.0),
         Nv(0.0),
         Nr(0.0),
         Nvvv(0.0),
         Nrvv(0.0),
         Nvrr(0.0),
         Nrrr(0.0),
+        Nphi(0.0),
+        Nphivv(0.0),
+        Nvphiphi(0.0),
+        Nphirr(0.0),
+        Nrphiphi(0.0),
+        Kphi(0.0),
+        Kphiphi(0.0),
         mx(0.0),
-        my(0.0)
+        my(0.0),
+        GM(0.0)
 {}
 
 std::string MMGManeuveringForceModel::model_name() {return "MMG maneuvering";}
@@ -54,21 +70,36 @@ MMGManeuveringForceModel::Input MMGManeuveringForceModel::parse(const std::strin
     node["Xrr"] >> ret.Xrr;
     node["Xvr"] >> ret.Xvr;
     node["Xvvvv"] >> ret.Xvvvv;
+    node["Xvphi"] >> ret.Xvphi;
+    node["Xrphi"] >> ret.Xrphi;
+    node["Xphiphi"] >> ret.Xphiphi;
     node["Yv"] >> ret.Yv;
     node["Yr"] >> ret.Yr;
     node["Yvvv"] >> ret.Yvvv;
     node["Yrvv"] >> ret.Yrvv;
     node["Yvrr"] >> ret.Yvrr;
     node["Yrrr"] >> ret.Yrrr;
+    node["Yphi"] >> ret.Yphi;
+    node["Yphivv"] >> ret.Yphivv;
+    node["Yvphiphi"] >> ret.Yvphiphi;
+    node["Yphirr"] >> ret.Yphirr;
+    node["Yrphiphi"] >> ret.Yrphiphi;  
     node["Nv"] >> ret.Nv;
     node["Nr"] >> ret.Nr;
     node["Nvvv"] >> ret.Nvvv;
     node["Nrvv"] >> ret.Nrvv;
     node["Nvrr"] >> ret.Nvrr;
     node["Nrrr"] >> ret.Nrrr;
+    node["Nphi"] >> ret.Nphi;
+    node["Nphivv"] >> ret.Nphivv;
+    node["Nvphiphi"] >> ret.Nvphiphi;
+    node["Nphirr"] >> ret.Nphirr;
+    node["Nrphiphi"] >> ret.Nrphiphi;
+    node["Kphi"] >> ret.Kphi;
+    node["Kphiphi"] >> ret.Kphiphi;
     node["mx"] >> ret.mx;
     node["my"] >> ret.my;
-
+    node["GM"] >> ret.GM;
 
     return ret;
 }
@@ -79,21 +110,28 @@ Wrench MMGManeuveringForceModel::get_force(const BodyStates& states, const doubl
     const double u = states.u();
     const double v = states.v();
     const double r = states.r();
+    const double phi = states.get_angles(env.rot).phi;
+    const double dphi_dt = states.p();
 
-    const double xG = states.G.v(0) - env.k->get(body_name, name).get_point().v(0); // The point in the transform is always P in body frame as per the input
-    const double vm = v - xG*r;
+    const double body_mass = states.solid_body_inertia(2,2);
+    const double xG = states.G.v(0) - env.k->get(body_name, name).get_point().v(0);
+    const double zG = states.G.v(2) - env.k->get(body_name, name).get_point().v(2);
+
+    const double vm = v - xG*r+zG*dphi_dt;
     const double U = hypot(u, vm);
     if (U!=0)
     {
         const double u_ = u/U;
         const double vm_ = vm/U;
         const double r_ = r*input.Lpp/U;
-        const double X = -input.R0 + input.Xvv*vm_*vm_ + input.Xrr*r_*r_ + input.Xvr*vm_*r_ + input.Xvvvv*vm_*vm_*vm_*vm_ + input.my*vm_*r_;
-        const double Y = input.Yv*vm_ + input.Yr*r_ + input.Yvvv*vm_*vm_*vm_ + input.Yvrr*vm_*r_*r_  + input.Yrrr*r_*r_*r_ + input.Yrvv*r_*vm_*vm_ - input.mx*u_*r_;
-        const double N = input.Nv*vm_ + input.Nr*r_ + input.Nvvv*vm_*vm_*vm_ + input.Nvrr*vm_*r_*r_  + input.Nrrr*r_*r_*r_ + input.Nrvv*r_*vm_*vm_;
-        tau(0) = 0.5*env.rho*pow(U,2)*input.Lpp*input.T*X;
-        tau(1) = 0.5*env.rho*pow(U,2)*input.Lpp*input.T*Y;
-        tau(5) = 0.5*env.rho*pow(U,2)*input.Lpp*input.Lpp*input.T*N;
+        const double X_ = -input.R0 + input.Xvv*vm_*vm_ + input.Xrr*r_*r_ + input.Xvr*vm_*r_ + input.Xvvvv*vm_*vm_*vm_*vm_ + input.Xvphi*vm_*phi + input.Xrphi*r_*phi + input.Xphiphi*phi*phi + input.my*vm_*r_;
+        const double Y_ = input.Yv*vm_ + input.Yr*r_ + input.Yvvv*vm_*vm_*vm_ + input.Yvrr*vm_*r_*r_  + input.Yrrr*r_*r_*r_ + input.Yrvv*r_*vm_*vm_ + input.Yphi*phi + input.Yphivv*phi*vm_*vm_ + input.Yvphiphi*vm_*phi*phi + input.Yphirr*phi*r_*r_ + input.Yrphiphi*r_*phi*phi - input.mx*u_*r_;
+        const double N_ = input.Nv*vm_ + input.Nr*r_ + input.Nvvv*vm_*vm_*vm_ + input.Nvrr*vm_*r_*r_  + input.Nrrr*r_*r_*r_ + input.Nrvv*r_*vm_*vm_ + input.Nphi*phi + input.Nphivv*phi*vm_*vm_ + input.Nvphiphi*vm_*phi*phi + input.Nphirr*phi*r_*r_ + input.Nrphiphi*r_*phi*phi;
+        const double K = - body_mass*env.g*input.GM*phi + input.Kphi*dphi_dt + input.Kphiphi * dphi_dt*abs(dphi_dt);
+        tau(0) = 0.5*env.rho*pow(U,2)*input.Lpp*input.T*X_;
+        tau(1) = 0.5*env.rho*pow(U,2)*input.Lpp*input.T*Y_;
+        tau(3) = K;
+        tau(5) = 0.5*env.rho*pow(U,2)*input.Lpp*input.Lpp*input.T*N_;
     }
     return Wrench(ssc::kinematics::Point(name, 0, 0, 0), body_name, tau);
 }
--- xdyn/force_models/MMGManeuveringForceModel.hpp
+++ xdyn/force_models/MMGManeuveringForceModel.hpp
@@ -6,6 +6,19 @@
 
 #include "xdyn/core/ForceModel.hpp"
 
+/** \details This class was created to define the maneuvering model used in the MMG model
+ * 
+ *  It was originally based on the 3 DOF MMG model as described in
+ * H. Yasukawa and Y. Yoshimura, “Introduction of MMG standard method for ship maneuvering predictions,” Journal of Marine Science and Technology, vol. 20, no. 1, pp. 37–52, Nov. 2014, doi: 10.1007/s00773-014-0293-y.
+ * 
+ * More recently, it has been extended to 4 DOF, as described in
+ * R. Okuda, H. Yasukawa, and A. Matsuda, “Validation of maneuvering simulations for a KCS at different forward speeds using the 4-DOF MMG method,” Ocean Engineering, vol. 284, p. 115174, Sep. 2023, doi: 10.1016/j.oceaneng.2023.115174.
+ * 
+ *  \addtogroup ForceModels
+ *  \ingroup module
+ *  \section 
+ *  \snippet module/unit_tests/MMGManeuveringForceModelTest.cpp
+ */
 class MMGManeuveringForceModel : public ForceModel
 {
 public:
@@ -20,20 +33,36 @@ public:
         double Xrr;
         double Xvr;
         double Xvvvv;
+        double Xvphi;
+        double Xrphi;
+        double Xphiphi;
         double Yv;
         double Yr;
         double Yvvv;
         double Yrvv;
         double Yvrr;
         double Yrrr;
+        double Yphi;
+        double Yphivv;
+        double Yvphiphi;
+        double Yphirr;
+        double Yrphiphi;        
         double Nv;
         double Nr;
         double Nvvv;
         double Nrvv;
         double Nvrr;
         double Nrrr;
+        double Nphi;
+        double Nphivv;
+        double Nvphiphi;
+        double Nphirr;
+        double Nrphiphi;
+        double Kphi;
+        double Kphiphi;
         double mx;//<! non-dimensional added mass coefficient a11
         double my;//<! non-dimensional added mass coefficient a22
+        double GM;
     };
     MMGManeuveringForceModel(const Input& input, const std::string& body_name, const EnvironmentAndFrames& env);
     MMGManeuveringForceModel() = delete;
